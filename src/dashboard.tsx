8620b014998e0cdb41f7aed5dcb87ae38afa07e3
ve11yn <angeline.vellyn@gmail.com>
2025-05-02 23:53:45 +0700

    Added Collapsible Navigation Bar

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -1,14 +1,16 @@
 import { UserContext } from "./entity/userContext";
 import { useContext } from "react";
+import Navigation2 from "./navigation2";
 
 const Dashboard = () => {
     const {state} = useContext(UserContext);
     return (
-        <div>
-        <h1>Hello, {state.name}</h1>
-        <p>Email: {state.email}</p>
-            <p>Phone: {state.phone}</p>
-            <p>Address: {state.address}, {state.city}, {state.state} {state.zip}</p>
+        <div className="page-container">
+            <Navigation2/>
+            <div className="dashboard">
+                <h3>Welcome Back,</h3>
+                <h1>{state.name}</h1>
+             </div>
         </div>
         
     );  
---
+++ src/navigation2.tsx
@@ -0,0 +1,40 @@
+import { useContext, useState } from "react";
+import { UserContext } from "./entity/userContext";
+import { Link } from "react-router-dom";
+import "./index.css";
+
+const Navigation2 = () => {
+    const {state} = useContext(UserContext);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    const toggleNavigation = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+
+    return (
+        <>
+            <button 
+                className="nav-toggle-btn"
+                onClick={toggleNavigation}
+            >
+                {isCollapsed ? "→" : "←"}
+            </button>
+            <div className={`navigation2 ${isCollapsed ? 'collapsed' : ''}`}>
+                <div>
+                    <p>Anggar.in</p>
+                </div>
+
+                <div className="nav-links-2">
+                    <Link to="/dashboard" className="nav-link">Dashboard</Link>
+                    <Link to="/budgetPlan" className="nav-link">Budget Plan</Link>
+                    <Link to="/fundRequest" className="nav-link">Fund Request</Link>
+                    <Link to="/Profile" className="nav-link">Profile</Link>
+                </div>
+
+                <button>Log Out</button>
+            </div>
+        </>
+    )
+};
+
+export default Navigation2;
